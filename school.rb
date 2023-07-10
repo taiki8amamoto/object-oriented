eebb619f870eae65d4983af6aba3677411680acc
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-10 11:54:58 +0900

    オブジェクト指向設計入門14まで完了

--- school.rb
+++ school.rb
@@ -1,13 +1,29 @@
 class School
-  def name
-    puts @greeting = "Hello!"
+  attr_accessor :name,
+                :address,
+                :number_of_students,
+                :founding_years,
+                :introduction_video_url,
+                :introducrion_statement
+
+  def initialize(name, address, number_of_students, founding_years, introduction_video_url, introducrion_statement)
+    @name = name
+    @address = address
+    @number_of_students = number_of_students
+    @founding_years = founding_years
+    @introduction_video_url = introduction_video_url
+    @introducrion_statement = introducrion_statement
   end
 
-  def name1
-    puts @greeting
+  def sample_instance_method
+    puts "sampleだよ！"
+    puts "#{@name}用のインスタンスだよ！"
   end
+
 end
 
-school = School.new
-school.name
-school.name1
+a_school = School.new("A学校", "東京都渋谷区..", 300 ,100, "https://hoge.com", "A学校は自然豊かな...")
+a_school.sample_instance_method
+
+b_school = School.new("B学校", "東京都新宿区..", 500, 30, "https://foo.com", "B学校は文武両立で...")
+b_school.sample_instance_method
--- test.rb
+++ test.rb
@@ -1,13 +1,27 @@
 class School
-  def name=(word)
-    @school_name = word
-  end
+  attr_accessor :name,
+                :address,
+                :number_of_students,
+                :founding_years,
+                :introduction_video_url,
+                :introducrion_statement
 
-  def name1
-    puts @school_name
+  def initialize(name, address, number_of_students, founding_years, introduction_video_url, introducrion_statement)
+    @name = name
+    @address = address
+    @number_of_students = number_of_students
+    @founding_years = founding_years
+    @introduction_video_url = introduction_video_url
+    @introducrion_statement = introducrion_statement
   end
 end
 
-  school = School.new
-  school.name="A学校"
-  school.name1
+a_school = School.new("A学校", "渋谷区..", 300 ,100, "https://hoge.com", "A学校は自然豊かな...")
+puts a_school.name
+puts a_school.address
+puts a_school.number_of_students
+puts a_school.founding_years
+puts a_school.introduction_video_url
+puts a_school.introducrion_statement
+
+puts a_school.instance_variables
